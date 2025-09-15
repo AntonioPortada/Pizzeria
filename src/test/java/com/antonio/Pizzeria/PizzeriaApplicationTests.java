0bb3a8e4585cbe7664ead2c6dab549e1bc6e7103
Portada Luna Jorge Antonio <jorge.portada@grupopakar.com.mx>
2025-09-15 15:55:26 -0600

    Initial commit

---
+++ src/test/java/com/antonio/Pizzeria/PizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonio.Pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
