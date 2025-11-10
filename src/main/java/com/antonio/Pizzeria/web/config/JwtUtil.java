4441bbf7ae73eb921bdeff9a4da04ca0c3307e95
Portada Luna Jorge Antonio <jorge.portada@grupopakar.com.mx>
2025-11-10 10:43:47 -0600

    Creating the JSON Web Token with Auth0 : vid-17

---
+++ src/main/java/com/antonio/Pizzeria/web/config/JwtUtil.java
@@ -0,0 +1,24 @@
+package com.antonio.Pizzeria.web.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class JwtUtil {
+
+    private static String SECRET_KEY = "mi_servidor_unico";
+    private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+
+    public String create(String username) {
+        return JWT.create()
+                .withSubject(username)
+                .withIssuer("platzi-pizza")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
+                .sign(ALGORITHM);
+    }
+}
